db5a8127bb274ad9051b8a31a73e619a1ee1a8ca
mejimaru <mejimaru@gmail.com>
2013-04-20 03:51:14 +0700

    inisial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteGw::Application.config.secret_token = '0ae6251aaa4eeeb9d13c01aee1a8e30b9db7f603528cce29229b079e8939b5e31cd1cc83683adafece121e2d39548720732564616679180a5506d9edd992df8c'
